5aba47325d6c27177ca96349a766a3bab67fbc9d
gambithovzla <gambithovzla@gmail.com>
2026-04-09 23:21:52 -0500

    Revert "Add gameday play-by-play detail tab"
    This reverts commit 65a62587c3c5f104367e299ae6ad8c976a2f0e65.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,6 @@ import BacktestDashboard  from './components/BacktestDashboard';
 import BacktestRunner     from './components/BacktestRunner';
 import DatasetDashboard  from './components/DatasetDashboard';
 import LiveTracker         from './components/LiveTracker';
-import GameDayDetail       from './components/GameDayDetail';
 import useHistory           from './hooks/useHistory';
 import { C, MONO, BARLOW } from './theme';
 
@@ -257,11 +256,6 @@ export default function App() {
           {/* Live Tracker */}
           {activeTab === 'live' && (
             <LiveTracker lang={lang} />
-          )}
-
-          {/* Gameday play-by-play detail */}
-          {activeTab === 'gameday' && (
-            <GameDayDetail lang={lang} />
           )}
 
           {/* Batch Scan (admin only) */}
